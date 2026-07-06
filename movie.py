10dd8428cec93b539c6c387a19004150a8dc4a24
GauravPathania18 <pathaniagaurav1818@gmail.com>
2026-07-06 14:39:59 +0000

    movie suggestion API

---
+++ movie.py
@@ -0,0 +1,40 @@
+from flask import Flask, jsonify
+import sqlite3
+import random
+
+app = Flask(__name__)
+
+movies_list = [
+    "The Shawshank Redemption",
+    "The Dark Knight",
+    "Inception",
+    "Interstellar",
+    "Pulp Fiction",
+    "The Matrix",
+    "Forrest Gump",
+    "Spirited Away",
+    "Gladiator",
+    "Avatar"
+]
+
+@app.route('/')
+def home():
+    return (
+        "<h1>Welcome to the Random Movie Suggestion API!</h1>"
+        "<p>To get a random movie suggestion, visit: "
+        "<code>/movie</code></p>"
+    )
+
+@app.route('/movie', methods=['GET'])
+def get_random_movie():
+    # Randomly select a movie from the list
+    suggested_movie = random.choice(movies_list)
+    
+    # Return the recommendation in JSON format
+    return jsonify({
+        "suggestion": suggested_movie
+    }), 200
+
+# Run the Flask app
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
